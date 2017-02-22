35245ffdfc1b266b5848f6a68dca10101cced561
AlexandreMelano <alexandremelano@yahoo.com>
2017-02-22 16:39:37 -0500

    second update'

--- update_database.php
+++ update_database.php
@@ -9,25 +9,19 @@ $bookGenre = filter_input(INPUT_POST, "GenreTextField");
 
 // check if user is Adding a New Book
 if($isAddition == "1") {
-
+    $query = "INSERT INTO books (Title, Author, Price, Genre) VALUES (:book_Title, :book_Author, :book_Price, :book_Genre)";
+}else
+{
+    $bookID = filter_input(INPUT_POST, "IDTextField"); // $_POST["IDTextField"];
 /*//////////////////////*/
 /* FIX THIS MYSQL QUERY */
 /*//////////////////////*/
 
-$query = "";
-$statement = $db->prepare($query); // encapsulate the sql statement
-}
-// else if user is Updating an Existing Book
-else {
-$bookID = filter_input(INPUT_POST, "IDTextField");
-
-/*//////////////////////*/
-/* FIX THIS MYSQL QUERY */
-/*//////////////////////*/
-
-$query = "";
-$statement = $db->prepare($query); // encapsulate the sql statement
-$statement->bindValue(':book_id', $bookID);
+/*/$query = "";
+$statement = $db->prepare($query); // encapsulate the sql statement/*/
+    $query = "UPDATE books SET Title = :book_Title, Author = :book_Author, Price = :book_Price, Genre = :book_Genre WHERE Id = :book_id "; // SQL statement
+    $statement = $db->prepare($query); // encapsulate the sql statement
+    $statement->bindValue(':book_id', $bookID);
 
 }
 
